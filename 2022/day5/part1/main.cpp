011b086ea6544917257dbe30f5f25bf175196ba0
Alexander Groszewski <groszewa@gmail.com>
2022-12-23 14:57:57 -0600

    complete day5

--- 2022/day5/part1/main.cpp
+++ 2022/day5/part1/main.cpp
@@ -1,14 +1,52 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<stack>
+#include<vector>
 using namespace std;
 
+
 int main() {
-    ifstream file("test.txt");
-    //ifstream file("input.txt");
+    //ifstream file("test.txt");
+    ifstream file("input.txt");
     string str;
-    while(getline(file, str)) {
-        cout << str << endl;
+    stack<string> initial_state;
+    do {
+        getline(file, str);
+        if(str[1]=='1') break;
+        initial_state.push(str);
+    } while(true);
+    vector<stack<char>> v;
+    v.push_back(stack<char>());
+    for(auto ch : str) if(isdigit(ch)) v.push_back(stack<char>());
+    while(not initial_state.empty()) {
+        string line = initial_state.top();
+        int index = 1;
+        for(int i=1; i<line.size(); i+=4) {
+            if(isalpha(line[i])) {
+                v[index].push(line[i]);
+            }
+            index++;
+        }
+        initial_state.pop();
     }
+    getline(file, str);
+    while(getline(file, str)) {
+        str = str.substr(str.find(' ')+1);
+        int count = stoi(str.substr(0,str.find(' ')));
+        str = str.substr(str.find(' ')+1);
+        str = str.substr(str.find(' ')+1);
+        int from  = stoi(str.substr(0,str.find(' ')));
+        str = str.substr(str.find(' ')+1);
+        str = str.substr(str.find(' ')+1);
+        int to    = stoi(str);
+        while(count) {
+            v[to].push(v[from].top());
+            v[from].pop();
+            count--;
+        }
+    }
+    for(int i=1; i<v.size(); i++) cout << v[i].top();
+    cout << endl;
     return 0;
 }
